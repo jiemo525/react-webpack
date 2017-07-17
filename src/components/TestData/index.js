ddc7b040ff253971d47980e8063c31a4cd921c77
jiemo525 <god_luogege@163.com>
2017-07-17 11:15:26 +0800

    update project

--- src/components/TestData/index.js
+++ src/components/TestData/index.js
@@ -1,7 +1,15 @@
-var Mock = require('mockjs');
-var data = Mock.mock({
-    'list|1-10': [{
-        'id|+1': 1
-    }]
-});
-console.log(JSON.stringify(data, null, 4))
+// var Mock = require('mockjs');
+// var data = Mock.mock({
+//     'list|1-10': [{
+//         'id|+1': 1
+//     }]
+// });
+// console.log(JSON.stringify(data, null, 4))
+// http://rapapi.org/mockjsdata/22534/test.do
+// import axios from 'axios';
+// axios.get('http://rapapi.org/mockjsdata/22534/test.do')
+//     .then(function(response) {
+//         console.log(response);
+//     }).catch(function (error) {
+//         console.log(error);
+//     });
